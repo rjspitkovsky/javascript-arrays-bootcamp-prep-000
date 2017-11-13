fa20de485270cb36b3acf7d143d7a1d2e458dff6
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-13 15:48:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,19 +8,19 @@ var ingredient6 = "garlic"
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-[element, ...array]
+return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+return   array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
-  [...array, element]
+return   [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
+  return array.push(element)
 }
 
 function accessElementInArray(array, index) {
