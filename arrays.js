b25b9ef20381c81d2bb11491d2874d1c7477fd8b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-13 15:49:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ return [element, ...array]
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
 array.unshift(element)
-return array 
+return array
 }
 
 function addElementToEndOfArray(array, element) {
@@ -21,7 +21,8 @@ return   [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array 
 }
 
 function accessElementInArray(array, index) {
