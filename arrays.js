03ffce4ee7380851257ad26face824f8c178c3e8
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-13 15:49:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,8 @@ return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-return   array.unshift(element)
+array.unshift(element)
+return array 
 }
 
 function addElementToEndOfArray(array, element) {
